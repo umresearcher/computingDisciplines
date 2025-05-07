e71cd0082a8e61f861023955a3bb49e04b80bb79
umresearcher <88212583+umresearcher@users.noreply.github.com>
2025-05-07 09:43:39 -0400

    Add files via upload

--- MarketBasketAnalysis/analysis.py
+++ MarketBasketAnalysis/analysis.py
@@ -14,13 +14,13 @@ def load_css(file_path):
         st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
 
 # Load the CSS file
-load_css('styles.css')
+load_css('MarketBasketAnalysis/styles.css')
 
 #add side for uploading file
 uploaded_file = st.sidebar.file_uploader("Upload a file")
 
 #default dataset: read the dataset
-transactions_df = pd.read_csv('my_transactions.csv')#change the path to the dataset
+transactions_df = pd.read_csv('MarketBasketAnalysis/my_transactions.csv')#change the path to the dataset
 
 if uploaded_file is not None:
     transactions_df = pd.read_csv(uploaded_file)
@@ -29,7 +29,7 @@ if uploaded_file is not None:
     
 #put a download button
 st.sidebar.markdown('Download the template') 
-st.sidebar.markdown('[The template](https://github.com/umresearcher/Market-Basket-Analysis/blob/main/my_transactions.csv)')#update the link to the dataset
+st.sidebar.markdown('[The template](https://github.com/umresearcher/computingDisciplines/blob/main/MarketBasketAnalysis/my_transactions.csv)')#update the link to the dataset
       
 #add title to the app
 st.title('Market Basket Analysis')
